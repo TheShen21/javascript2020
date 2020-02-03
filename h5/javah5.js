c979b3ab32846cbf0fe4a802416850ddd6b7eca4
theshen <bshen@roc-dev.com>
2020-02-03 11:10:22 +0100

    H4 done

---
+++ h5/javah5.js
@@ -0,0 +1,36 @@
+pics = document.getElementById("pic");
+createPicsHolders();
+createAapImages();
+function createPicsHolders() {
+    for(var i = 0; i< 9; i++){
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "picture-holder";
+        pictureHolder.id = "picture-holder-" + i;
+        pics.appendChild(pictureHolder);
+    }
+}
+
+function createAapImages() {
+    pictureHolders = document.getElementsByClassName("picture-holder");
+    for (var i = 0; i < pictureHolders.length; i++){
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet_"+ i;
+        aapPlaatje = document.createElement("img");
+        aapPlaatje.src = "img/aap"+ (i+1) +".jpg";
+        aapPlaatje.id = i;
+        aapPlaatje.addEventListener("click", function(){
+         maakFavoriet(this.id);
+        });
+        pictureHolders[i].appendChild(favoriet);
+        pictureHolders[i].appendChild(aapPlaatje);
+    }
+}
+function maakFavoriet(id) {
+    notsofavoriet = document.getElementsByClassName("favoriet");
+    for(var i = 0; i <notsofavoriet.length; i++){
+        notsofavoriet[i].style.backgroundImage= "none";
+    }
+    favoriet = document.getElementById("favoriet_" + id);
+    favoriet.style.backgroundImage ="url('img/hartje.jpg')";
+}
